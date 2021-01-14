3d31a2f1b329082131db0209e9ee7d342598d647
RenatoLomba <63821245+RenatoLomba@users.noreply.github.com>
2021-01-14 14:02:20 -0300

    calculadora finalizada

--- calculadora-react/src/main/Calculator.jsx
+++ calculadora-react/src/main/Calculator.jsx
@@ -3,24 +3,93 @@ import Button from '../components/Button'
 import Display from '../components/Display'
 import './Calculator.css'
 
+//ESTADO INICIAL DA CALCULADORA
+const initialState = {
+    displayValue: '0', //VALOR NO DISPLAY
+    clearDisplay: false, //LIMPAR DISPLAY
+    operation: null, //OPERAÇÃO
+    values: [0, 0], //VALORES QUE SERÃO UTILIZADOS
+    current: 0 //POSIÇÃO ATUAL DO ARRAY
+}
+
 export default class Calculator extends Component {
 
+    state = { ...initialState }
+
     clearMemory = () => {
-        console.log('limpar')
+        this.setState({ ...initialState })
     }
 
-    setOperation = (op) => {
-        console.log(op)
+    setOperation = (operation) => {
+        //SETAR OPERAÇÃO, LIMPAR O DISPLAY
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+
+            //EXECUTA OS CÁLCULOS
+            switch (currentOperation) {
+                case '+':
+                    values[0] = values[0] + values[1]
+                    break;
+                case '-':
+                    values[0] = values[0] - values[1]
+                    break;
+                case '*':
+                    values[0] = values[0] * values[1]
+                    break;
+                case '/':
+                    values[0] = values[0] / values[1]
+                    break;
+                default:
+                    break;
+            }
+            values[1] = 0
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
 
     addDigit = (n) => {
-        console.log(n)
+        //CASO O VALOR DIGITADO FOR . E JA HOUVER UM . NO DISPLAY, ELE IGNORA
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        //CASO O VALOR DO DISPLAY SEJA 0 OU O ESTADO SEJA VERDADEIRO, LIMPA O VALOR DO DISPLAY
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
+
+        //VALOR DO DISPLAY ATUAL, OU VAZIO
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+
+        //VALOR ATUAL DO DISPLAY CONCATENADO COM O VALOR DIGITADO
+        const displayValue = currentValue + n
+
+        this.setState({ displayValue, clearDisplay: false })
+
+        //ARMAZENAR O VALOR DIGITADO NO ESTADO
+        if (n !== '.') {
+            const index = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[index] = newValue
+            this.setState({ values })
+        }
     }
 
     render() {
         return (
             <div className="Calculator">
-                <Display value={100} />
+                <Display value={this.state.displayValue} />
                 <Button click={this.clearMemory} triple label="AC" />
                 <Button click={this.setOperation} operation label="/" />
                 <Button click={this.addDigit} label="7" />
